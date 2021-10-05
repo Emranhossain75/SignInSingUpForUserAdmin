47e5200b40f74768577fffa3444a9c1eee71dfe5
Emran <emran53hossain@gmail.com>
2021-10-05 13:53:07 +0600

    Admin And User Login

---
+++ LoginRegistrationForUserAdmin/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using LoginRegistrationForUserAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LoginRegistrationForUserAdmin.Controllers
+{
+    public class AccountController : Controller
+    {
+        SignInSignUpEntities db = new SignInSignUpEntities();
+        // GET: Account
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login( SignUp sign)
+        {
+            using (var context = new SignInSignUpEntities())
+            {
+                FormsAuthentication.SetAuthCookie(sign.UserName, false);
+                bool Isvalid = context.SignUps.Any(x => x.UserName == "arafatmollik" && x.Password == sign.Password);
+                ViewBag.Message = "Welcome Admin";
+                
+
+                if (Isvalid) 
+                { 
+
+                    return RedirectToAction("Admin", "Home");
+
+                }
+                bool Isvalid1 = context.SignUps.Any(x => x.UserName == "User" && x.Password == sign.Password);
+                if (Isvalid1)
+                {
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Invalid UserName And Password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(SignUp model)
+        {
+            using (var context = new SignInSignUpEntities())
+            {
+                context.SignUps.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+        
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login");
+        }
+    
+}
+}
---
+++ LoginRegistrationForUserAdmin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistrationForUserAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "arafatmollik")]
+        // GET: Home
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        [Authorize(Roles = "User,arafatmollik")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
